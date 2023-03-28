f2f8acc763128b6861a1dc492ce458279bc526bc
orangelDev <orangeleando@gmail.com>
2023-03-28 09:31:44 -0500

    Resolviendo las relaciones uno a uno

--- services/customers.service.js
+++ services/customers.service.js
@@ -23,7 +23,7 @@ class CustomerService {
   async create(data) {
     const newCustomer = await models.Customer.create(data, {
       include: ['user']
-    });
+      });
     return newCustomer;
   }
 
