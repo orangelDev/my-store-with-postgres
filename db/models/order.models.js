2bc0d1e042c7fa8da0a023c41cdf0560c059adb6
orangelDev <orangeleando@gmail.com>
2023-04-06 23:33:54 -0500

    Resolviendo relaciones muchos a muchos

--- db/models/order.models.js
+++ db/models/order.models.js
@@ -34,6 +34,20 @@ const OrderSchema = {
     type: DataTypes.DATE,
     field: 'created_at',
     defaultValue: Sequelize.NOW
+  },
+  total: {
+    type: DataTypes.VIRTUAL,
+    get() {
+      if (this.items) {
+        if (this.items.length > 0) {
+          return this.items.reduce((total, item) => {
+            return total + (item?.price * item?.OrderProduct.amount)
+          }, 0)
+        }
+        return 0
+      }
+      return 0
+    }
   }
 }
 
@@ -41,7 +55,13 @@ class Order extends Model {
   static associate(models){
     this.belongsTo(models.Customer, {
       as: 'customer',
-    })
+    });
+    this.belongsToMany(models.Product, {
+      as: 'items',
+      through: models.OrderProduct,
+      foreignKey: 'orderId',
+      otherKey: 'productId',
+    });
   }
   static config(sequelize){
     return{
